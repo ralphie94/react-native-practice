2a51a4f4578018ee7bc38e4e2b3f88ae002bc378
Ralphie Celedon <ralphie@Ralphies-MacBook-Pro.local>
2019-09-06 01:34:40 -0700

    react native jsx practice

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,13 +1,22 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View } from "react-native";
 
 const ComponentsScreen = () => {
-    return <Text style={styles.textStyle}>This is the components screen</Text>;
-};
+    const name = "Ralphie";
 
+    return (
+        <View>
+            <Text style={styles.textStyle}>Getting started with react native</Text>
+            <Text style={styles.subHeaderStyle}>My name is {name}</Text>
+        </View>
+    );
+};
 const styles = StyleSheet.create({
     textStyle: {
-        fontSize: 30
+        fontSize: 45
+    },
+    subHeaderStyle: {
+        fontSize: 20
     }
 });
 
